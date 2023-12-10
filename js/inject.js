f7af62691f4fb855db7b7620721de282ed54df69
Entrpix <korr626@outlook>
2023-12-10 12:13:26 -0500

    blocked -> blockedURLs

--- js/inject.js
+++ js/inject.js
@@ -1,17 +1,17 @@
-const blocked = [`example.com`];
+const blockedURLs = ['example.com'];
 const blockedTLDs = ['.org'];
 const currentURL = window.location.href;
 
 // Site Block Check
 function isBlocked(url, blockedList) {
     for (const blockedItem of blockedList) {
-        const regex = new RegExp(`^(?:[a-zA-Z0-9-]+:\\/\\/)?(?:[a-zA-Z0-9-]+\\.)?${blockedItem.replace(/\./g, '\\.')}`, 'i'); // Regex is scawy D:
+        const regex = new RegExp(`^(?:[a-zA-Z0-9-]+:\\/\\/)?(?:[a-zA-Z0-9-]+\\.)?${blockedItem.replace(/\./g, '\\.')}`, 'i');
         if (url.match(regex)) {
             return true;
-        };
-    };
+        }
+    }
     return false;
-};
+}
 
 // TLD Block Check
 function isBlockedTLD(url, blockedTLDList) {
@@ -20,17 +20,17 @@ function isBlockedTLD(url, blockedTLDList) {
         const regex = new RegExp(`\\${blockedTLD}$`, 'i');
         if (domain.match(regex)) {
             return true;
-        };
-    };
+        }
+    }
     return false;
-};
+}
 
 // Block Site
-if (isBlocked(currentURL, blocked)) {
+if (isBlocked(currentURL, blockedURLs)) {
     chrome.runtime.sendMessage({ action: 'block' });
-};
+}
 
 // Block TLD
 if (isBlockedTLD(currentURL, blockedTLDs)) {
     chrome.runtime.sendMessage({ action: 'block' });
-};
+}
